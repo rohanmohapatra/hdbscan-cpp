08ab34cdaadc883226e32f450ee4e932bd1d6105
Your Name <you@example.com>
2019-06-19 11:04:47 +0530

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "utils/example.hpp"
+using namespace std;
+using namespace csv;
+
+int main() {
+	csv_reader reader("dataset.csv");
+	vector<vector <double>> dataset = reader.read_csv(2, false);
+	for (int i = 0; i < dataset.size(); i++) {
+		for (int j = 0; j < dataset[i].size(); j++) {
+			cout << dataset[i][j] << ",";
+		}
+		cout << endl;
+	}
+	return 0;
+}
