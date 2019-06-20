d3fc5b9a38e241b26793ff135bb638da20c9ec0c
Your Name <you@example.com>
2019-06-20 09:36:35 +0530

    Changed Directory Structure

---
+++ HDBSCAN-CPP/distance/ManhattanDistance.hpp
@@ -0,0 +1,10 @@
+#include"IDistanceCalculator.hpp"
+/// <summary>
+/// Computes the manhattan distance between two points, d = |x1-y1| + |x2-y2| + ... + |xn-yn|.
+/// </summary>
+class ManhattanDistance : IDistanceCalculator
+{
+public:
+	double computeDistance(std::vector<double> attributesOne, std::vector<double> attributesTwo);
+};
+
--- HDBSCANUnitTest/HDBSCANUnitTest.cpp
+++ HDBSCANUnitTest/HDBSCANUnitTest.cpp
@@ -2,12 +2,64 @@
 #include <iostream>
 #include "CppUnitTest.h"
 #include"../HDBSCAN-CPP/distance/EuclideanDistance.hpp"
+#include"../HDBSCAN-CPP/Example/example.hpp"
 #include <limits>
+#include <typeinfo>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace HDBSCANUnitTest
 {
+	TEST_CLASS(ExampleTest)
+	{
+	public:
+		TEST_METHOD(CheckFileNameTest)
+		{
+			string expectedFileName = "../HDBSCANDataset/Dataset.csv";
+			Example example("../HDBSCANDataset/Dataset.csv");
+			string fileName = example.getFileName();
+			Assert::AreEqual(expectedFileName, fileName);
+		}
+		TEST_METHOD(CheckDatasetTypeTest)
+		{
+			Example example(".../../HDBSCANDataset/Dataset.csv");
+			example.loadCsv(2);
+			string expectedClassName = "class std::vector<class std::vector<double,class std::allocator<double> >,class std::allocator<class std::vector<double,class std::allocator<double> > > >";
+			Assert::AreEqual(expectedClassName, (string)typeid(example.dataset).name());
+		}
+		TEST_METHOD(FileNotFoundErrorTest)
+		{
+			Example example("../HDBSCANDataset/Dataset2.csv");
+			int realLoaded = example.loadCsv(2);
+			Assert::AreEqual(0, realLoaded);
+		}
+		TEST_METHOD(FilePresentTest)
+		{
+			Example example("../../HDBSCANDataset/Dataset.csv");
+			int realLoaded = example.loadCsv(2);
+			Assert::AreEqual(1, realLoaded);
+		}
+		TEST_METHOD(LoadCsvWithHeaderTest)
+		{
+			Example example("../../HDBSCANDataset/Dataset.csv");
+			example.loadCsv(2,true);
+			Assert::AreEqual(99, (int)example.dataset.size());
+		}
+		TEST_METHOD(LoadCsvWithoutHeaderTest)
+		{
+			Example example("../../HDBSCANDataset/Dataset.csv");
+			example.loadCsv(2);
+			Assert::AreEqual(100, (int)example.dataset.size());
+		}
+		TEST_METHOD(CsvNumberOfAttributesTest)
+		{
+			Example example("../../HDBSCANDataset/Dataset.csv");
+			example.loadCsv(1);
+			Assert::AreEqual(1, (int)example.dataset[0].size());
+		}
+	};
+	
+	
 	TEST_CLASS(EuclideanTest)
 	{
 	public:
