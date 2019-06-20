bfd3c7070a423ba8b8f89fdfb61cafdeaaa48f50
Your Name <you@example.com>
2019-06-20 09:36:35 +0530

    Changed Directory Structure

---
+++ HDBSCAN-CPP/Example/example.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include<string>
+#include<vector>
+#include"../Runner/hdbscanResult.hpp"
+
+using namespace std;
+
+
+class Example
+
+{
+
+private:
+
+	string fileName;
+
+
+
+public:
+
+	vector < vector <double > > dataset;
+
+	hdbscanResult result;
+
+
+	Example(string readFileName) {
+
+		fileName = readFileName;
+
+	}
+
+	string getFileName();
+			   
+	int loadCsv(int numberOfValues, bool skipHeader=false);
+
+	void execute();
+
+	void displayResult();
+
+
+};
+
